ff35ddc36672a91d3bc1f9778cff1d6ccc9b85d6
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-06-14 22:25:31 +0200

    adding ticket card

--- app/tickets/[id]/TicketDetail.tsx
+++ app/tickets/[id]/TicketDetail.tsx
@@ -1,15 +1,67 @@
 import { Ticket } from '@prisma/client'
 import React from 'react'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import TicketStatusBadge from '@/components/TicketStatusBadge'
+import TicketPriority from '@/components/TicketPriority'
+import Link from 'next/link'
+import { buttonVariants } from '@/components/ui/button'
 
 interface Props {
     ticket: Ticket
 }
 
 export default function TicketDetail({ticket}: Props) {
+
+
   return (
-    <div>
-        <p>{ticket.title}</p>
-        <p>{ticket.description}</p>
+    <div className='lg:grid lg:grid-cols-4'>
+        <Card className='mx-4 mb-4 lg:col-span-3 lg:mr-4'>
+            <CardHeader>
+                <div className='flex justify-between mb-3'>
+                    <TicketStatusBadge status={ticket.status} />
+                    <TicketPriority priority={ticket.priority} />
+                </div>
+                <CardTitle>{ticket.title}</CardTitle>
+                <CardDescription>Created: {ticket.createdAt.toLocaleDateString("en-US", {
+                    year: "2-digit",
+                    month: "2-digit",
+                    day: "2-digit",
+                    hour: "numeric",
+                    minute: "2-digit",
+                    hour12: true
+                })}</CardDescription>
+                <CardAction>Card Action</CardAction>
+            </CardHeader>
+            <CardContent>
+                <p>{ticket.description}</p>
+            </CardContent>
+            <CardFooter>
+                Updated: {ticket.createdAt.toLocaleDateString("en-US", {
+                    year: "2-digit",
+                    month: "2-digit",
+                    day: "2-digit",
+                    hour: "numeric",
+                    minute: "2-digit",
+                    hour12: true
+                })}
+            </CardFooter>
+        </Card>
+        <div className='mx-4 flex lg:flex-col lg:mx-0 gap-3'>
+            <Link href={`/tickets/edit/${ticket.id}`} className={`${buttonVariants({
+                variant: "default"
+            })}`} >Edit Ticket</Link>
+            <Link href={`/tickets/edit/${ticket.id}`} className={`${buttonVariants({
+                variant: "default"
+            })}`} >Delete Ticket</Link>
+        </div>
     </div>
   )
 }
--- components/TicketPriority.tsx
+++ components/TicketPriority.tsx
@@ -14,10 +14,10 @@ const priorityMap: Record<Priority, {label: string, level: 1 | 2 | 3}> = {
 
 export default function TicketPriority({priority}: Props) {
   return (
-    <>
+    <div className='flex justify-between'>
       <Flame className={`${priorityMap[priority].level >=1 ? "text-red-500" : "text-muted"}`}/>
       <Flame className={`${priorityMap[priority].level >=2 ? "text-red-500" : "text-muted"}`}/>
       <Flame className={`${priorityMap[priority].level >=3 ? "text-red-500" : "text-muted"}`}/>
-    </>
+    </div>
   )
 }
