3cf8b05cdfa089bcd1246ae60094958fbdf80300
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-06-14 19:31:36 +0200

    creating update ticket form and updating ticket

---
+++ app/tickets/edit/[id]/page.tsx
@@ -0,0 +1,20 @@
+import TicketForm from "@/components/TicketForm";
+import React from "react";
+
+interface Props {
+  params: { id: string };
+}
+
+const EditTicket = async ({ params }: Props) => {
+  const ticket = await prisma?.ticket.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!ticket) {
+    return <p className=" text-destructive">Ticket not found!</p>;
+  }
+
+  return <TicketForm ticket={ticket} />;
+};
+
+export default EditTicket;
--- components/TicketForm.tsx
+++ components/TicketForm.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react'
 import { Form, FormControl, FormField, FormItem, FormLabel } from './ui/form'
 import { z } from 'zod'
@@ -11,10 +13,15 @@ import { Select, SelectTrigger, SelectContent, SelectItem, SelectValue } from '.
 import { Button } from './ui/button';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { Ticket } from '@prisma/client';
 
 type TicketFormData = z.infer<typeof ticketsSchema>;
 
-export default function TicketForm() {
+interface Props {
+    ticket?: Ticket
+}
+
+export default function TicketForm({ticket}: Props) {
 
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [error, setError] = useState("");
@@ -26,26 +33,30 @@ export default function TicketForm() {
     });
 
     async function onSubmit(values:z.infer<typeof ticketsSchema>) {
-        try {
+         try {
             setIsSubmitting(true);
             setError("");
 
-            await axios.post("/api/tickets", values);
+            if (ticket) {
+                await axios.patch("/api/tickets/" + ticket.id, values);
+            } else {
+                await axios.post("/api/tickets", values);
+            }
+            setIsSubmitting(false);
             router.push("/tickets");
             router.refresh();
-
         } catch (error) {
-            setError("Unknown Error Occured.")
-            setIsSubmitting(false)
+            console.log(error);
+            setError("Unknown Error Occured.");
+            setIsSubmitting(false);
         }
-        console.log(values);
     }
 
     return (
         <div className='rounded-md border w-full p-4'>
         <Form {...form} >
             <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8 w-full'>
-                <FormField control={form.control} name='title' render={({field}) => (
+                <FormField control={form.control} defaultValue={ticket?.title} name='title' render={({field}) => (
                     <FormItem>
                         <FormLabel>Ticket Title</FormLabel>
                         <FormControl>
@@ -53,17 +64,17 @@ export default function TicketForm() {
                         </FormControl>
                     </FormItem>
                 )} />
-                <Controller name='description' control={form.control} render={({field}) => (
+                <Controller name='description' control={form.control} defaultValue={ticket?.description} render={({field}) => (
                     <SimpleMDE placeholder='description' {...field} />
                 )} />
                 <div className='flex w-full space-x-4'>
-                    <FormField control={form.control} name='status' render={({field}) => (
+                    <FormField control={form.control} defaultValue={ticket?.status} name='status' render={({field}) => (
                         <FormItem>
                             <FormLabel>Status</FormLabel>
                             <Select onValueChange={field.onChange} defaultValue={field.value}>
                                 <FormControl>
                                     <SelectTrigger>
-                                        <SelectValue placeholder="Status..." />
+                                        <SelectValue placeholder="Status..." defaultValue={ticket?.status}/>
                                     </SelectTrigger>
                                 </FormControl>
                                 <SelectContent>
@@ -74,13 +85,13 @@ export default function TicketForm() {
                             </Select>
                         </FormItem>
                     )} />
-                    <FormField control={form.control} name='priority' render={({field}) => (
+                    <FormField control={form.control} name='priority' defaultValue={ticket?.priority} render={({field}) => (
                         <FormItem>
                             <FormLabel>Priority</FormLabel>
                             <Select onValueChange={field.onChange} defaultValue={field.value}>
                                 <FormControl>
                                     <SelectTrigger>
-                                        <SelectValue placeholder="Priority..." />
+                                        <SelectValue placeholder="Priority..." defaultValue={ticket?.priority} />
                                     </SelectTrigger>
                                 </FormControl>
                                 <SelectContent>
@@ -92,7 +103,7 @@ export default function TicketForm() {
                         </FormItem>
                     )} />
                 </div>
-                <Button type='submit' disabled={isSubmitting} className='cursor-pointer'>Submit</Button>
+                <Button type='submit' disabled={isSubmitting} className='cursor-pointer'>{ticket ? "Update Ticket" : "Create Ticket"}</Button>
             </form>
         </Form>
         </div>
