de84e3affac606489197043ed0078e76b8cdbba1
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-06-16 21:31:09 +0200

    update user option

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -31,32 +31,3 @@ export async function POST(request:NextRequest) {
     return NextResponse.json(newUser, {status: 201})
 
 }
-
-export async function PATCH(request:NextRequest) {
-    const body = await request.json();
-    const validation = userSchema.safeParse(body);
-    
-    if(!validation.success) {
-        return NextResponse.json(validation.error.format(), {status: 400})
-    }
-
-    const duplicate = await prisma.user.findUnique({
-        where: {
-            username: body.username
-        }
-    })
-
-    if(duplicate) {
-        return NextResponse.json({message: "Duplicate username"}, {status: 400})
-    }
-
-    const hashPassword = await bcrypt.hash(body.password, 10)
-    body.password = hashPassword;
-    
-    const newUser = await prisma.user.create({
-        data: {... body}
-    })
-
-    return NextResponse.json(newUser, {status: 201})
-
-}
