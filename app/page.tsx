cb8936bf34f80eae0c735e44c61c60c70a7109f7
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-06-18 19:40:10 +0200

    Dashboard Setup - adding recent tickets

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import prisma from "@/prisma/db";
+import DashRecentTickets from "@/components/DashRecentTickets";
 
-export default function page() {
+const Dashboard = async () => {
+  const tickets = await prisma.ticket.findMany({
+    where: {
+      NOT: [{ status: "CLOSED" }],
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+    skip: 0,
+    take: 5,
+    include: {
+      assignedToUser: true,
+    },
+  });
+
+  const groupTicket = await prisma.ticket.groupBy({
+    by: ["status"],
+    _count: {
+      id: true,
+    },
+  });
+
+  const data = groupTicket.map((item) => {
+    return {
+      name: item.status,
+      total: item._count.id,
+    };
+  });
+
   return (
     <div>
-      Dashboard
+      <div className="grid gap-4 md:grid-cols-2 px-2">
+        <div>
+          <DashRecentTickets tickets={tickets} />
+        </div>
+        <div>
+          {/* <DashChart data={data} /> */}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Dashboard;
---
+++ components/DashCarts.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DashCarts() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
