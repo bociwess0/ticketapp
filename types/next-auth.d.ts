2595d06186fa27a950525c754dad0f5e41a4e743
bociwess0 <bogdandjordjevic2000@gmail.com>
2025-06-18 18:33:50 +0200

    building next auth API

---
+++ types/next-auth.d.ts
@@ -0,0 +1,24 @@
+import { Role } from "@prisma/client";
+import NextAuth, {DefaultSession} from "next-auth";
+
+declare module "next-auth" {
+    interface Session {
+        user: {
+            username: string,
+            role: string;
+        } & DefaultSession["user"]
+    }
+
+    interface User {
+        id: number,
+        name: string,
+        username: string,
+        role: Role
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        role?: string
+    }
+}
